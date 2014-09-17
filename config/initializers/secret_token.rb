0b3e1fb025a715a2e12051828805b5af9726f38a
Marty Mavis <marty.mavis@csgi.com>
2014-09-17 14:02:19 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ElasticRailsPlayground::Application.config.secret_token = '559ccc65798a1f08c6927faa066fb8b02c82849b51f51d7aea4a35af6a3ed22617a438587239e0767798626c8a416da856277393d41483a619969e54c159cd3c'
